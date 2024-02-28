776fe33cc068c4b3180e5ef8e957cfb2f9c362f5
Maxwell Jung <jungmaxwell@gmail.com>
2024-02-28 12:37:19 -0800

    oauth with battlenet
    commit 92e111474bba849438c077864c0f54ea2105b3ec
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Wed Feb 28 12:32:03 2024 -0800
    
        fix indents
    
    commit 6c05712b4238b57274643cb8ee2e38909affb54b
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Wed Feb 28 02:53:40 2024 -0800
    
        modularize bot and app
    
    commit aa9bb0c52a08a02def7d7a421d677e76a371f7e8
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Wed Feb 28 00:00:43 2024 -0800
    
        working localhost bnet oauth

---
+++ app.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const BnetStrategy = require('passport-bnet').Strategy;
+
+require('dotenv').config();
+const BNET_ID = process.env.BNET_OAUTH_CLIENT_ID;
+const BNET_SECRET = process.env.BNET_OAUTH_CLIENT_SECRET;
+const OAUTH_CALLBACK_URL = process.env.OAUTH_CALLBACK_URL || 'http://localhost:3000/oauth/battlenet/callback';
+// Review full list of available scopes here: https://develop.battle.net/documentation/guides/using-oauth
+const OAUTH_SCOPES = process.env.OAUTH_SCOPES || 'openid';
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+// Register the BnetStrategy within Passport.
+passport.use(
+    new BnetStrategy(
+        { clientID: BNET_ID,
+          clientSecret: BNET_SECRET,
+          scope: OAUTH_SCOPES,
+          callbackURL: OAUTH_CALLBACK_URL,
+          state: btoa(generateRandomString()) },
+        function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function() {
+            return done(null, profile);
+        });
+    })
+);
+
+const app = express();
+
+// configure Express
+app.use(cookieParser());
+app.use(session({ secret: 'passport-battlenet-example', // Change this value to a unique value for your application!
+                  saveUninitialized: true,
+                  resave: true }));
+
+// Initialize Passport! Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/oauth/battlenet',
+        passport.authenticate('bnet'));
+
+app.get('/oauth/battlenet/callback',
+        passport.authenticate('bnet', { failureRedirect: '/' }),
+        function(req, res){
+        res.redirect('/');
+        });
+
+app.get('/', function(req, res) {
+    if (req.isAuthenticated()) {
+        const output = `
+        <html>
+            <body>
+            <h1>Express Passport-Bnet OAuth Example</h1>
+            <table>
+                <tr>
+                <th>Account ID</th>
+                <th>Battletag</th>
+                </tr>
+                <tr>
+                <td>${req.user.id}</td>
+                <td>${req.user.battletag}</td>
+                </tr>
+            </table>
+            <br />
+            <a href="/logout">Logout</a>
+            </body>
+        </html>
+        `;
+        res.send(output);
+    } else {
+        const output = `
+        <html>
+            <body>
+            <h1>Express Passport-Bnet OAuth Example</h1>
+            <br />
+            <a href="/oauth/battlenet">Login with Bnet</a>
+            </body>
+        </html>
+        `;
+        res.send(output);
+    }
+});
+
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err);
+    res.send("<h1>Internal Server Error</h1>");
+});
+
+const server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+function generateRandomString() {
+	let randomString = '';
+	const randomNumber = Math.floor(Math.random() * 10);
+
+	for (let i = 0; i < 20 + randomNumber; i++) {
+		randomString += String.fromCharCode(33 + Math.floor(Math.random() * 94));
+	}
+
+	return randomString;
+}
